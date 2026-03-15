3077946fa8352b02ab553cef3c18c84faacb28a8
Eyanur islam <mdeyanurislam784@gmail.com>
2026-03-15 17:09:15 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,87 @@
+"""
+Malik = Eyanur
+Tarikh = 15 Mar 2026
+"""
+
+age=18
+if age >=18:
+    print("tumi vot parbe   ")
+else:
+    print("tumi vot dite parbe na")
+na=20
+if na >5:
+    print("nambar bro")
+else:
+    print("nambar choto")
+num =20
+if num>10:
+    print("bro")
+else:
+    print("choto")
+nun=30
+if nun>36:
+    print("b0ro")
+else:
+    print("choto")
+nam =55
+if nam >764:
+    print("boro")
+else:
+    print("chto")
+sa=763
+if sa <7655:
+    print("boro")
+else:
+    print("chto")
+vv=7645
+if vv>353:
+    print("boro ")
+else:
+    print("choto")
+ff=4343434
+if ff>7653:
+    print("boro")
+else:
+    print("choto")
+dsv=345
+if dsv >6535:
+    print("boro")
+else:
+    print("choto")
+fb=65
+if fb>6:
+    print("boro")
+else:
+    print("choto")
+z=242
+if z<3552:
+    print("boro")
+else:
+    print("choto")
+x=35
+if x >34:
+    print("boro")
+else:
+    print("choto")
+c=6532
+if c >652:
+    print("boro")
+else:
+    print("choto")
+v=2365
+if v>65326532:
+    print("boro")
+else:
+    print("choto")
+b=532
+if b >3245:
+    print("boro")
+else:
+    print("choto")
+j=6532
+if j >6532:
+    print("boro")
+else:
+    print("coto")
+    
+    
