ecbfc21c56b5a0db9794b5a1776d6161e50f4913
Eyanur islam <mdeyanurislam784@gmail.com>
2026-03-16 15:45:14 +0600

    Create mar16-2026.py

---
+++ mar16-2026.py
@@ -0,0 +1,25 @@
+nan="eyanur"
+ 
+
+if nan=="nasim":
+    print("vai take salam daw")
+    
+elif nan=="nifad":
+    print("kos ase take aste daw")
+    
+elif nan=="rupom":
+    print("kaje ase aste daw")
+    
+else:
+    print("wo china kew hole pori choy ho")
+    
+nam="rupom"
+
+if nam =="eyanur":
+    print("malik asce")
+elif nam=="jahanur":
+    print("amar bro vai")
+elif nam=="nifad":
+    print("amar kos asce ")
+else:
+    print("ke jany asce ")
