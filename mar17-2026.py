0efbc81020881d62d41695640cf924a7fd2098c6
Eyanur islam <mdeyanurislam784@gmail.com>
2026-03-17 15:35:55 +0600

    Update mar17-2026.py

--- mar17-2026.py
+++ mar17-2026.py
@@ -18,3 +18,44 @@ elif name == "jemi ":
 else:
     print("wono kew")
 print("jdscd")
+
+for i in range(5):
+    print("eyanur")
+else:
+    print(i)
+names =["eyanur ","nifad","jaHANUR","nasim"]
+for n in names:
+    print(n[0])
+    print(names[3])
+    print(names[2])
+    print(n[1])
+    print(n[2])
+    print(n[3])
+    print(names[1])
+    print(names[0])
+nann =["nifad","nasim","shohan","jahanur"]
+for bbn in nann:
+    print(bbn)
+    print(nann)
+na =[6,2,32,3,4,5,67,]
+for b in na:
+    print(b)
+    print(na)
+nj=[21,43,45,32,46,]
+for n in nj:
+    print(n)
+    print(nj)
+bc=[76,324,344,3245,532,543]
+for fj in bc:
+    print (fj)
+    print(bc)
+jx=["e","w","r",'p']
+for kj in jx:
+    print(kj)
+    print(kj)
+for i in range(100):
+    print("nifad")
+name ["for","god",'nana']
+for n in name:
+    print(n)
+print("finish")
