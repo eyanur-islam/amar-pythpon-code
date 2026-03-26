da8b01de5340fa87cc07bcd3f0bff40ba633de2c
Eyanur islam <mdeyanurislam784@gmail.com>
2026-03-26 11:26:36 +0600

    Create mar26-2026.py

---
+++ mar26-2026.py
@@ -0,0 +1,21 @@
+name = "nifad "
+if name == "nasim":
+    print ("amar bro vai")
+else:
+    print("amar ro bro vai ")
+age= 18
+if age>=18:
+    print("tumi vot dete pare")
+else :
+    print("tumi vot dite parbe na ")
+name =["eyanur","jahanur","nifad"]
+print(name)
+print(name [1])
+print(name[0])
+print(name[2])
+bajar=["madila","fulbari","jinajpur:"]
+print(bajar[0])
+thana=["madilahat","fulbari","dinajpur"]
+print(thana[1])
+kos=input("nam ki :")
+input("basa kothay :")
